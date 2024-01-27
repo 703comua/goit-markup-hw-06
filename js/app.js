816bd495c6e91a6c83d7ff3cfff095108f22fcfc
Sergey <52292140+703comua@users.noreply.github.com>
2024-01-27 20:20:33 +0200

    mob-menu, modal, js

---
+++ js/app.js
@@ -0,0 +1,40 @@
+(() => {
+    const refs = {
+        openModalBtn: document.querySelectorAll("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
+
+    refs.openModalBtn.forEach((element) => {
+        element.addEventListener("click", toggleModal);
+    });
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    //* Закривання по кліку на бекдроп
+
+    refs.modal.addEventListener("click", removeModal);
+    function removeModal(e) {
+        if (e.target === e.currentTarget) {
+            refs.modal.classList.add("is-hidden");
+        }
+    }
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
+	}
+	
+	document.querySelector(".hero__btn").addEventListener("click", toggleOrderServiceModal);
+	document.querySelector(".modal__close-btn").addEventListener("click", toggleOrderServiceModal);
+    document.querySelector(".backdrop").addEventListener("click", removeOrderServiceModal);
+;
+	function toggleOrderServiceModal() {
+        document.querySelector(".backdrop").classList.toggle("is-open");
+        document.body.classList.toggle("no-scroll");
+	}
+	
+	function removeOrderServiceModal() {
+		document.querySelector(".backdrop").classList.remove("is-open");
+		document.body.classList.remove("no-scroll");
+	}
+})();
